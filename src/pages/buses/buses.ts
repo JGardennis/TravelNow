335bfcd8cb73bdfb983f0376088f16ce7a04e407
Joshua Dennis <joshuagardennis@gmail.com>
2018-03-26 18:19:31 +0100

    Installed Ionic-Native Geolocation plugin for transport API calls

--- src/pages/buses/buses.ts
+++ src/pages/buses/buses.ts
@@ -1,6 +1,29 @@
+// ********************************************
+// IMPORTS
+// ********************************************
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController,
   ModalController, ViewController } from 'ionic-angular';
+
+import { Geolocation } from '@ionic-native/geolocation';
+
+// ********************************************
+// CLASSES
+// ********************************************
+class Bus {
+  direction;
+  time_Hour;
+  time_Minute;
+  bus_line;
+
+  constructor(dir: string, hr: string, min: string, line: string) {
+    this.direction = dir;
+    this.time_Hour = hr;
+    this.time_Minute = min;
+    this.bus_line = line;
+  }
+}
+
 
 @Component({
   selector: 'page-buses',
@@ -8,8 +31,41 @@ import { IonicPage, NavController, NavParams, LoadingController,
 })
 export class BusPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public loadCtrl: LoadingController,
+    public location: Geolocation) {
 
+  }
+
+  /************************
+  @name:      ionViewDidLoad
+  @desc:      runs function as soon as page is loaded
+  ************************/
+  ionViewDidLoad()
+  {
+    console.log('Bus Page Loaded...');
+
+    let loadPrompt = this.loadCtrl.create({
+      content: 'Finding nearest buses...'
+    });
+
+    loadPrompt.present();
+  }
+
+  /************************
+  @name:      initBusData
+  @desc:      creates xml link to initialise Transport API with geolocation
+  ************************/
+  initBusData()
+  {
+    //get user's location
+    this.location.getCurrentPosition().then((response) => {
+      var lat = response.coords.latitude;
+      var long = response.coords.longitude;
+
+      
+    })
   }
 
 }
